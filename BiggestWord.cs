764411b9816f096b9f8e39466a8e0d2a57809f99
Jacob Hubbard <68961999+jacobrhubbard@users.noreply.github.com>
2020-11-08 13:13:47 -0500

    Add files via upload

---
+++ BiggestWord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LongestWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //C:\Users\Jacob\Desktop\words_alpha.txt
+            //Ask for location of the file
+            Console.WriteLine("Location of file that you want to be read");
+            string filename = Console.ReadLine();
+            string[] words = System.IO.File.ReadAllLines(@filename);
+            string biggestWord = "";
+            foreach (string word in words)
+            {
+                if(word.Length >= biggestWord.Length)
+                {
+                    biggestWord = word;
+                }
+            }
+            Console.WriteLine(biggestWord);
+        }
+    }
+}
